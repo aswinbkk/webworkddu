d93ef828f9616d17e06d5d45ba4e9ef3c8b50242
aswin <aswinbaburajkk@gmail.com>
2026-04-07 11:15:09 +0530

    commit

--- practices/array_method/array_method.js
+++ practices/array_method/array_method.js
@@ -1,35 +1,35 @@
 const arr = [1,24.68,8673.95,436,8];
 
-  // Largest number in an array.
+// Largest number in an array.
   const maxNum = Math.max(...arr);
   console.log(`Largest number = ${maxNum}`);
 
-  // Create a new array with each number doubled.
+// Create a new array with each number doubled.
   const mapDouble = arr.map(variable => variable*2);
   console.log(`Doubled numbers = ${mapDouble}`);
 
-  // Given an array of objects, use map() to get all names.
+// Given an array of objects, use map() to get all names.
   let objectArray = [ {name: "name1", marks: 10}, {name: "name2", marks: 20} ];
   let mapName = objectArray.map(variable => variable.name);
   console.log(mapName);
 
-  // To get all even numbers from an array.
+// To get all even numbers from an array.
   const filtterEven = arr.filter(variable => variable % 2 === 0)
   console.log(`Even numbers = ${filtterEven}`);
 
-  // Check if an element exists in an array.
+// Check if an element exists in an array.
   const includesExists = arr.includes(8673.95)
   console.log(`Even exists in an array = ${includesExists}`);
 
-  // Sort an array in ascending order.
+// Sort an array in ascending order.
   arr.sort((a,b) => a-b);
   console.log(`Sorted array in ascending order = ${arr}`);
 
-  // Sum of all elements in an array.
+// Sum of all elements in an array.
   const reduceSum = arr.reduce((total, current) => total + current, 0);
   console.log(`Sum of the given array = ${reduceSum}`);
 
-  // Print only even numbers from an array.
+// Print only even numbers from an array.
   let forOfEven=[];
   for (let variable of arr) {
     if (variable % 2 === 0) {
