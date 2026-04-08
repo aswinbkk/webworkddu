7ea3290bc6f6083a99914afac8834a445f412205
aswin <aswinbaburajkk@gmail.com>
2026-04-08 14:01:51 +0530

    final commit

--- aswin_exam_02-04-2026/10.sum_elements.js
+++ aswin_exam_02-04-2026/10.sum_elements.js
@@ -1,3 +1,3 @@
 const arr = [10, 3.04, 4766, 216.60];
-const reduceSum = arr.reduce((accumulator, variableCurrent) => accumulator + variableCurrent, 0);
+const reduceSum = arr.reduce((accu, varCurrent) => accu + varCurrent, 0);
 console.log(`Sum of the given array = ${reduceSum}`);
