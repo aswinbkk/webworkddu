31ee7873a43657c921845474e3df14a0e2369f1d
aswin <aswinbaburajkk@gmail.com>
2026-04-08 14:05:53 +0530

    final commit

--- aswin_exam_02-04-2026/9.check_element.js
+++ aswin_exam_02-04-2026/9.check_element.js
@@ -1,3 +1,3 @@
 const arr = [10, 3.04, 4766, 216.60];
-const includesExists = arr.includes(8673.95);
+const includesExists = arr.includes(216.60);
 console.log(`Given element exists = ${includesExists}`);
