1873eb18ff3d60d5c9e200301e8e004f89019004
aswin <aswinbaburajkk@gmail.com>
2026-04-22 15:59:14 +0530

    user model

---
+++ acewyn/backend/models/userModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name : 
+    {
+        type : String,
+        required : true
+    },
+    email : 
+    {
+        type : String,
+        required : true
+    },
+    phone : 
+    {
+        type : String,
+        required : true
+    },
+    dob : 
+    {
+        type : String,
+        required : true
+    }
+},{timestamps:true})
+
+const user = mongoose.model('users', userSchema)
+module.exports = user;
