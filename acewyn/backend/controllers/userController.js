1b1bfaabd0783f605f70b75270d6aac0db27943b
aswinbkk <aswinbaburajkk@gmail.com>
2026-04-23 23:21:19 +0530

    commit

--- acewyn/backend/controllers/userController.js
+++ acewyn/backend/controllers/userController.js
@@ -1,9 +1,17 @@
-const user = require('../models/userModel');
+const user = require('../models/userModel')
+const bcrypt = require('bcrypt')
+const saltround = 10;
 
 const createUser = async (req,res) => {
-    const {name,email,password,phone,dob} = req.body;
+    const {name,email,password,phone,dob} = req.body
 
     try {
+
+        const user= await user.findone({email});
+        if(user){
+            return res.status(400).json({msg:"user exists"})
+        }
+        const hashPassword = await bcrypt.hash(password,saltround)
         const newData = await new user({
             name,
             email,
