2cc328e3ddbafc3970e16aeb602e4972240b381e
aswin <aswinbaburajkk@gmail.com>
2026-05-12 11:45:55 +0530

    commit

--- mern-app/frontend/src/components/Search.jsx
+++ mern-app/frontend/src/components/Search.jsx
@@ -2,6 +2,33 @@ import styled from 'styled-components'
 import React, { useEffect, useState } from 'react';
 import { getData } from '../api/apiSearch';
 
+const Container = styled.div`
+display:flex;
+gap:20px;
+padding:10px;
+`;
+
+const Card = styled.div`
+width:200px;
+height:100px;
+background-color:#fffdd0;
+border-radius:10px;
+padding:10px;
+text-align:center;
+`;
+const Title = styled.p`
+font-size:16px;
+font-weight:600;
+`;
+const Description = styled.p`
+font-size:16px;
+font-weight:500;
+`;
+const Author = styled.p`
+font-size:14px;
+font-weight:500;
+`;
+
 const Search = () => {
     const [data, setData] = useState([]);
 
@@ -19,16 +46,15 @@ const Search = () => {
     }, []);
 
     return (
-        <div>
-            <h1>Search</h1>
+        <Container>
             {data.map((item, index) => (
-                <div key={index}>
-                    <h2>{item.title}</h2>
-                    <p>{item.description}</p>
-                    <p>{item.author}</p>
-                </div>
+                <Card key={index}>
+                    <Title>{item.title}</Title>
+                    <Description>{item.description}</Description>
+                    <Author>{item.author}</Author>
+                </Card>
             ))}
-        </div>
+        </Container>
     )
 }
 
